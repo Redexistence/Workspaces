9f5038d1890c7f5b2b735f3f0b470d98e92ebde8
Redexistence <thefamousredexistence@gmail.com>
2025-08-04 20:39:21 -0400

    KeenKiz CCC

--- Lesson August 4th/EX 306.py
+++ Lesson August 4th/EX 306.py
@@ -1,2 +1,14 @@
-When analyzing data collected as part of a science experiment it may be desirable to remove the most extreme values before performing other calculations.
-Write a function that takes a list of values and an non-elements
+def remove_outliers(data, n):
+    if len(data) < 2 * n:
+        raise ValueError("List must contain at least {} elements.".format(2 * n))
+    sorted_data = sorted(data)
+    return sorted_data[n:-n]
+
+user_input = input("Enter a list of numbers separated by spaces: ")
+numbers = [float(x) for x in user_input.split()]
+if len(numbers) < 4:
+    print("Error: You must enter at least 4 numbers.")
+    exit()
+filtered = remove_outliers(numbers, 2)
+print("List with outliers removed:", filtered)
+print("Original list:", numbers)
