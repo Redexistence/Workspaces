60a3ed2e84ef854c1c63b74cd2ae630dd6904a8f
Redexistence <thefamousredexistence@gmail.com>
2025-08-04 21:02:37 -0400

    KeenKiz CCC

---
+++ Lesson August 4th/EX 138.py
@@ -0,0 +1,27 @@
+import random
+
+def create_bingo_card():
+    bingo = {}
+    ranges = {
+        'B': range(1, 16),
+        'I': range(16, 31),
+        'N': range(31, 46),
+        'G': range(46, 61),
+        'O': range(61, 76)
+    }
+    
+    for letter, number_range in ranges.items():
+        bingo[letter] = random.sample(number_range, 5)
+    
+    return bingo
+
+def display_bingo_card(bingo):
+    print(" B   I   N   G   O")
+    for i in range(5):
+        for letter in "BINGO":
+            print(f"{bingo[letter][i]:2}", end="  ")
+        print()
+
+
+card = create_bingo_card()
+display_bingo_card(card)
