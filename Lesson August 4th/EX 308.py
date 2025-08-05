cf4697f735821e97677ca998af16f1afa792d040
Redexistence <thefamousredexistence@gmail.com>
2025-08-04 20:53:10 -0400

    KeenKiz CCC

--- Lesson August 4th/EX 308.py
+++ Lesson August 4th/EX 308.py
@@ -1,23 +1,19 @@
-def categorize_numbers():
-    negative = []
-    zeros = []
-    positive = []
+negative = []
+zeros = []
+positive = []
 
-    while True:
-        user_input = input("Enter integer (or press Enter to finish): ")
-        if user_input == "":
-            break
-        number = int(user_input)
-        if number < 0:
-            negative.append(number)
-        elif number == 0:
-            zeros.append(number)
-        else:
-            positive.append(number)
+while True:
+    user_input = input("Enter integer (or press Enter to finish): ")
+    if user_input == "":
+        break
+    number = int(user_input)
+    if number < 0:
+        negative.append(number)
+    elif number == 0:
+        zeros.append(number)
+    else:
+        positive.append(number)
 
-    print("Negatives:", negative)
-    print("Zeros:", zeros)
-    print("Positives:", positive)
-
-
-categorize_numbers()
+print("Negatives:", negative)
+print("Zeros:", zeros)
+print("Positives:", positive)
