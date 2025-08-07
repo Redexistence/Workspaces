19e9a51fe04996f6a932bd2713accc95a61d21ab
Redexistence <thefamousredexistence@gmail.com>
2025-08-06 20:54:49 -0400

    KeenKiz CCC

--- Lesson August 6th/EX 312.py
+++ Lesson August 6th/EX 312.py
@@ -1,1 +1,30 @@
-Write a program that reads numbers from user until a blank line is entered. Your program should display the average of all the values entered by the user. The the program should display all of the below average values, followed by all of the average values (if any), followed by all of the above values. An appropriate label should be displayed before each list of values.
+numbers = []
+
+while True:
+    user_input = input("Enter a number (or press Enter to finish): ")
+    if user_input == "":
+        break
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        print("Invalid number.")
+
+if numbers:
+    average = sum(numbers) / len(numbers)
+    print(f"\nAverage: {average:.2f}")
+
+    below_average = [num for num in numbers if num < average]
+    average_values = [num for num in numbers if num == average]
+    above_average = [num for num in numbers if num > average]
+
+    print("\nBelow average values:")
+    print(below_average if below_average else "None")
+    
+    print("\nAverage values:")
+    print(average_values if average_values else "None")
+
+    print("\nAbove average values:")
+    print(above_average if above_average else "None")
+else:
+    print("None entered.")
